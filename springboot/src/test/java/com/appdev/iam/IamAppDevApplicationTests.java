d96abe2c8f29a38a3b81dd21072d627f00b29d91
omindu <omindu.dishan@gmail.com>
2023-07-12 18:10:50 +0530

    Cleaning file structure

---
+++ springboot/src/test/java/com/appdev/iam/IamAppDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamAppDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
